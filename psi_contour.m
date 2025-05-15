4dacb8bd2a22750b8bd9ec9d85428dfc55855adc
rw708 <123331823+rw708@users.noreply.github.com>
2025-05-15 12:09:03 +0100

    1

--- psi_contour.m
+++ psi_contour.m
@@ -1,2 +1,34 @@
-addpath('utils');
-result = psipv(1, 2, 3, 4, 5);
+clear
+close all
+
+nx = 51;
+ny = 41;
+xmin = -5;
+xmax = 5;
+ymin = -5;
+ymax = 5;
+xc = 0;
+yc = 0;
+Gamma = 1;
+
+
+for i=1:nx
+    for j=1:ny
+        xm(i,j) = xmin + (i-1)*(xmax-xmin)/(nx-1);
+    end
+end
+
+for i=1:nx
+    for j=1:ny
+        ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
+    end
+end
+
+for i=1:nx
+    for j=1:ny
+        psi(i,j) = psipv(xc,yc,Gamma,xm(i,j),ym(i,j));
+    end
+end
+
+c = -0.4:0.2:1.2;
+contour(xm,ym,psi,c)
