819db77ecafcbc81211f9384f3ca7a3d348c556c
Alexdggb <lj392@cam.ac.uk>
2025-05-15 12:52:44 +0100

    inf

--- inf_contour.m
+++ inf_contour.m
@@ -9,16 +9,26 @@ ymin = -2;
 ymax = 2;
 
 del = 1.5;
+Gamma = 3.0; 
 xm = zeros(nx, ny);
 ym = zeros(nx, ny);
 a = zeros(nx, ny);
 b = zeros(nx, ny);
+psi = zeros(nx, ny);
+
+nv = 100;
 
 for i=1:nx
     for j=1:ny
         xm(i,j) = xmin + (i-1)*(xmax-xmin)/(nx-1);
         ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
         [a(i,j),b(i,j)] = refpaninf(del,xm(i,j),ym(i,j));
+        psi(i,j) = 0;
+        for n = i:nv+1
+            xc = n*del/nv;
+            yc = 0; 
+            psi(i,j) = psi(i,j) + psipv(xc,yc,Gamma,xm(i,j),ym(i,j));
+        end
     end
 end
 
@@ -26,3 +36,5 @@ c = -0.15:0.05:0.15;
 contour(xm,ym,a,c);
 figure;
 contour(xm,ym,b,c);
+figure;
+contour(xm,ym,psi,c)
