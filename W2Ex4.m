99e7a7a21759deb5fdfedf6c2d2f806ed4e037d5
rw708 <123331823+rw708@users.noreply.github.com>
2025-05-22 12:25:56 +0100

    Week2 Ex4

--- W2Ex4.m
+++ W2Ex4.m
@@ -10,3 +10,27 @@ duedx = 0;
 x0 = 0.01;
 thick0(1) = 0.037*x0*(Re*x0)^(-1/5);
 thick0(2) = 1.80*thick0(1);
+
+[delx, thickhist] = ode45(@thickdash,[0 0.99],thick0);
+
+theta7 = zeros(length(delx),1);
+theta9 = zeros(length(delx),1);
+
+x = x0 + delx;
+
+for i = 1:length(delx)
+    theta7(i) = 0.037 * x(i) * (Re * x(i))^(-1/5);
+    theta9(i) = 0.023 * x(i) * (Re * x(i))^(-1/6);
+end
+
+mycolors = [1 0 0; 0 1 0; 0 0 1];
+
+hold on
+plot(x,thickhist(:,1))
+plot(x,theta7)
+plot(x,theta9)
+legend('theta','theta7', 'theta9','Location','southeast')
+ax = gca;
+ax.ColorOrder = mycolors;
+ylabel('θ/L')
+xlabel('x/L') 
--- utils/thickdash.m
+++ utils/thickdash.m
@@ -2,7 +2,7 @@ function dthickdx = thickdash(xmx0,thick)
 
 global Re ue0 duedx
 
-He = thick(2)/Thick(1);
+He = thick(2)/thick(1);
 
 if He >= 1.46
     H = (11*He + 15)/(48*He - 59);
@@ -14,6 +14,14 @@ ue = ue0 + duedx * xmx0;
 
 Rethet = Re * ue * thick(1);
 
-cf = 0.091448 * ((H-1) * Rethet)^(-0.232) * e^(-1.26*H);
+cf = 0.091448 * ((H-1) * Rethet)^(-0.232) * exp(-1.26*H);
 
-cdiss = 
+cdiss = 0.010025 * ((H-1) * Rethet)^(-1/6);
+
+a = cf/2 - (H+2) * duedx * thick(1)/ue;
+
+b = cdiss - 3 * duedx * thick(2)/ue;
+
+dthickdx = zeros(2,1);
+dthickdx(1,1) = a;
+dthickdx(2,1) = b;
