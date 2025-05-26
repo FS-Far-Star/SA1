3b0a9212edc0f4e51caccd5f40f5a60eff7a7594
Alexdggb <lj392@cam.ac.uk>
2025-05-26 09:18:24 +0100

    Update W2Ex5.m

--- W2Ex5.m
+++ W2Ex5.m
@@ -5,14 +5,13 @@ global Re ue0 duedx
 
 Re = 10^7;
 ue0 = 1;
-duedx = -0.6;
+duedx = -0.25;
 
 x0 = 0.01;
 thick0(1) = 0.037*x0*(Re*x0)^(-1/5);
 thick0(2) = 1.80*thick0(1);
 
 [delx, thickhist] = ode45(@thickdash,[0 0.99],thick0);
-
 
 He = thickhist(:,2)./thickhist(:,1);
 He = He(:);
@@ -22,12 +21,17 @@ cross_idx = find((He(1:end-1) > threshold_He) & (He(2:end) <= threshold_He), 1);
 x = x0 + delx;
 threshold = ones(length(x))*threshold_He;
 
+cross_flag = true;
 if ~isempty(cross_idx)
     idx_before = cross_idx;
     idx_after = cross_idx + 1;
+else
+    cross_flag = false;
 end
 
-fprintf('Separation occurs between x/L = %.2e and x/L = %.2e\n', x(idx_before), x(idx_after));
+if cross_flag == true
+    fprintf('Separation occurs between x/L = %.2e and x/L = %.2e\n', x(idx_before), x(idx_after));
+end
 
 mycolors = [1 0 0; 0 0 1];
 
