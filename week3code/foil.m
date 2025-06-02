b49f055a9aba314b0c658e574d6e237d369e5ec6
Alexdggb <lj392@cam.ac.uk>
2025-06-02 09:50:45 +0100

    stuff

--- week3code/foil.m
+++ week3code/foil.m
@@ -174,3 +174,39 @@ end
 
 fname = ['Data/' caseref '.mat'];
 save ( fname, 'xs', 'ys', 'alpha', 'clswp', 'cdswp', 'lovdswp' )
+
+
+% Load the .mat file
+data = load(['Data/' caseref '.mat']);
+
+% Inspect variable names
+disp(fieldnames(data));
+
+% Assuming variable names are: alpha, cl, cd
+alpha = data.alpha;  % Angle of attack (degrees)
+cl = data.clswp;        % Lift coefficient
+cd = data.cdswp;        % Drag coefficient
+
+% Plot Cl vs Alpha
+figure;
+plot(alpha, cl, 'b-', 'LineWidth', 2);
+xlabel('\alpha (deg)');
+ylabel('C_L');
+title('Lift Coefficient vs. Angle of Attack');
+grid on;
+
+% Plot Cd vs Alpha
+figure;
+plot(alpha, cd, 'r-', 'LineWidth', 2);
+xlabel('\alpha (deg)');
+ylabel('C_D');
+title('Drag Coefficient vs. Angle of Attack');
+grid on;
+
+% Plot Cd vs Cl
+figure;
+plot(cl, cd, 'r-', 'LineWidth', 2);
+xlabel('C_L');
+ylabel('C_D');
+title('Drag Coefficient vs. Lift Coefficient');
+grid on;
