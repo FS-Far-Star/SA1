e0f960e509fd70d0f2fd6c91930edb4d0a511d1f
rw708 <123331823+rw708@users.noreply.github.com>
2025-06-05 12:28:50 +0100

    Create wing_plot.m

---
+++ wing_plot.m
@@ -0,0 +1,54 @@
+clear, close all
+
+xmax = 1.0;
+xmin = 0.0;
+ymax =  .2;
+ymin = -.2;
+
+pathin=[pwd,'/Geometry/'];
+[filein,pathin]=uigetfile([pathin '*.surf']);
+y=load([pathin,filein]);
+x=y(2:end-1,1);
+y=y(2:end-1,2);
+L=length(x);
+I=(x-1).^2+y.^2; I=find(I==max(I)); I=I(1);
+Xbk=x;
+Ybk=y;hZ=[];
+axisZ=[];
+aZ=[];
+Res=get(0); Res=Res.ScreenSize;
+h=figure('units','normalized',...
+    'outerposition',[.05 .2 1.35*Res(4)/Res(3) .6],...
+    'DockControls','off',...
+    'MenuBar','none',...
+    'name','Wing Analysis Section Generator (Matlab version)',...
+    'NumberTitle','off');
+a=axes('position',[.10,.10,.87,.87]);
+[xs ys] = splinefit([1;x;1],[0;y;0],0);
+
+pathin_1=[pwd,'/Geometry/'];
+[filein,pathin_1]=uigetfile([pathin_1 '*.surf']);
+y_1=load([pathin_1,filein]);
+x_1=y_1(2:end-1,1);
+y_1=y_1(2:end-1,2);
+L=length(x_1);
+I=(x_1-1).^2+y_1.^2; I=find(I==max(I)); I=I(1);
+Xbk=x_1;
+Ybk=y_1;hZ=[];
+axisZ=[];
+aZ=[];
+Res=get(0); Res=Res.ScreenSize;
+h=figure('units','normalized',...
+    'outerposition',[.05 .2 1.35*Res(4)/Res(3) .6],...
+    'DockControls','off',...
+    'MenuBar','none',...
+    'name','Wing Analysis Section Generator (Matlab version)',...
+    'NumberTitle','off');
+a=axes('position',[.10,.10,.87,.87]);
+[xs_1 ys_1] = splinefit([1;x_1;1],[0;y_1;0],0);
+
+hold on
+plot(xs,ys,'k', 'Color',[1 0 0]);
+plot(xs_1,ys_1,'k', 'Color',[0 0 1]);
+axis equal
+axis([xmin xmax ymin ymax])
